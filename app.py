d938523b2e77e0814d9b2a7b6e7ef58e2401b080
Ariclenes6 <ariclenesobsilva@gmail.com>
2025-07-11 23:35:20 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("🚀 HighFlyer App")
+st.subheader("Monitoramento de velas altas 🔥")
+st.write("O aplicativo está funcionando! Agora você pode adicionar lógica de alerta, odds, e muito mais.")
